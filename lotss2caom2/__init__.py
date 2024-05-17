b61a97011a220919b7d227fc8718fee4be1c5d04
Sharon Goliath <Sharon.Goliath@nrc-cnrc.gc.ca>
2024-05-17 09:13:19 -0700

    CADC-13010 - fix the __init__.py file.

--- lotss2caom2/__init__.py
+++ lotss2caom2/__init__.py
@@ -2,6 +2,6 @@ from .clients import *  # noqa
 from .composable import *  # noqa
 from .data_source import *  # noqa
 from .fits2caom2_augmentation import *  # noqa
+from .lotss_execute import *  # noqa
 from .main_app import *  # noqa
-from .metadata_reader import *  # noqa
 from .preview_augmentation import *  # noqa
